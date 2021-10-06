63e2110a20842aa0061fa3184cc94ee09173f7ac
Nick <nstobbart@protonmail.com>
2021-10-06 16:33:35 +0100

    one.py comparespPython2 to python3

---
+++ one.py
@@ -0,0 +1,18 @@
+'''
+Some examples of working with Python2 vs Python3
+'''
+# if using Python2 would have to use raw_input whereas in python3 just need input.
+
+#ip_addr = input("enter an IP address: ")
+
+#altnertive way is:
+
+#first try python2 method
+try:
+    ip_addr2 = raw_input("Enter another IP Address: ")
+    # Then try Python3 method
+except NameError:
+    ip_addr2 = input("Enter Another IP Address:")
+
+#print(ip_addr)
+print(ip_addr2)
