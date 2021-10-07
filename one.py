2aef76719708b250fb2787471f39323870d1aae5
Nick <nstobbart@protonmail.com>
2021-10-07 22:45:05 +0100

    Add some boolen verification

--- one.py
+++ one.py
@@ -14,6 +14,17 @@ ip_a = '1.1.1.1'
 ip_b = '8.8.8.8'
 ip_c = '8.8.4.4'
 
+ipvsix_a=str("1001:0000:3238:DFE1:0063:0000:0000:FEFB")
+IPVSIX_B=str("2001:0000:3238:DFE1:0063:0000:0000:FEFB")
+IPv6_C=str("3001:0000:3238:DFE1:0063:0000:0000:FEFB")
+
+print(ipvsix_a)
+print(IPVSIX_B)
+print(IPv6_C)
+
+print("Does IPv6_a and IPv6_b match?: " ,ipvsix_a == IPVSIX_B)
+print("Does IPv6_a note match IPv6_b ?: " ,ipvsix_a != IPVSIX_B)
+
 ip_addr = input("enter an IP address: ")
 
 #print(ip_addr)
