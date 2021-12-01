fd967e1e83d181645afea28bfdf5235ccb688098
Nick <nstobbart@protonmail.com>
2021-12-01 17:10:43 +0000

    Work on spliting 1 function to a couple of sub functions.

--- cmd_output/cmd_outputv2.py
+++ cmd_output/cmd_outputv2.py
@@ -1,12 +1,11 @@
 from netmiko import Netmiko
 import click
 
-
 @click.command()
 @click.option('-c', '--command', help='Command to run on devices', required=True)
 @click.option('-u', '--username', help='Username to login to device', required=True)
 @click.option('--password', prompt=True, hide_input=True)
-def connection(command,username,password):
+def the_devices(command,username,password):
     device_list = 'device_list.txt'
     with open(device_list) as f:
         # remove \n whitespace from list.
@@ -19,16 +18,25 @@ def connection(command,username,password):
             'secret': password,
             'device_type': 'cisco_asa'
         }
-        #print(netdevice)
+        # print(netdevice)
+        return net_device
+
+def connection():
+        net_device = the_devices()
         net_conn = Netmiko(**net_device)
         output = net_conn.send_command(command)
         print('\n\n --- Logging into IP -', ip, "\n")
         print(net_conn.find_prompt())
-        print(output)
-        # write contents to file
-        f = open("cmd_output.txt", mode="w")
-        f.write(output)
-        f.flush()
+        save_output()
+        return output
 
+def save_output():
+    print(output)
+    # write contents to file
+    f = open("cmd_output.txt", mode="w")
+    f.write(output)
+    f.flush()
 
 connection()
+
+## above currently not working, no errors on run, just no output. Further troubleshooting tomorrow.
