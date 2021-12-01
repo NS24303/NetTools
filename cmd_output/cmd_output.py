db6599bfa065a21e7e5178cc88c2d812f54e1d43
Nick <nstobbart@protonmail.com>
2021-12-01 16:20:15 +0000

    Removed the need for getpass() by using click --password option.

--- cmd_output/cmd_output.py
+++ cmd_output/cmd_output.py
@@ -1,12 +1,11 @@
 from netmiko import Netmiko
-from getpass import getpass
 import click
 
 @click.command()
 @click.option('-c', '--command', help='Command to run on devices', required=True)
 @click.option('-u', '--username', help='Username to login to device', required=True)
-def connection(command,username):
-    password = getpass()
+@click.option('--password', prompt=True, hide_input=True)
+def connection(command,username,password):
     device_list = 'device_list.txt'
     with open(device_list) as f:
         # remove \n whitespace from list.
