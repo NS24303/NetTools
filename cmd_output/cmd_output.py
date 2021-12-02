d5bd78b5be298ae6e21560c7428468aa7ca97ed4
Nick <nstobbart@protonmail.com>
2021-12-02 15:54:02 +0000

    Modified the script to use 3 functions rather than having all the code in 1. Code is exactly the same apart from this.

--- cmd_output/cmd_output.py
+++ cmd_output/cmd_output.py
@@ -2,15 +2,21 @@ from netmiko import Netmiko
 import click
 
 
-@click.command()
-@click.option('-c', '--command', help='Command to run on devices', required=True)
-@click.option('-u', '--username', help='Username to login to device', required=True)
-@click.option('--password', prompt=True, hide_input=True)
-def connection(command,username,password):
+def load_devices():
     device_list = 'device_list.txt'
     with open(device_list) as f:
         # remove \n whitespace from list.
         devices = f.read().splitlines()
+        # print(devices)
+    return devices
+
+
+@click.command()
+@click.option('-c', '--command', help='Command to run on devices', required=True)
+@click.option('-u', '--username', help='Username to login to device', required=True)
+@click.option('--password', prompt=True, hide_input=True)
+def connection(command, username, password):
+    devices = load_devices()
     for ip in devices:
         net_device = {
             'host': ip,
@@ -19,16 +25,19 @@ def connection(command,username,password):
             'secret': password,
             'device_type': 'cisco_asa'
         }
-        #print(netdevice)
         net_conn = Netmiko(**net_device)
-        output = net_conn.send_command(command)
-        print('\n\n --- Logging into IP -', ip, "\n")
-        print(net_conn.find_prompt())
-        print(output)
-        # write contents to file
-        f = open("cmd_output.txt", mode="w")
-        f.write(output)
-        f.flush()
+        save_output(net_conn, command, ip)
+
+
+def save_output(net_conn, command, ip):
+    output = net_conn.send_command(command)
+    print('\n\n --- Logging into IP -', ip, "\n")
+    print(net_conn.find_prompt())
+    print(output)
+    # write contents to file
+    f = open("cmd_output.txt", mode="w")
+    f.write(output)
+    f.flush()
 
 
 connection()
